3dfb4791c2b9a33287b2d412ae0afecb67fb1e2a
vasishchev <dmitry.vasishev@gmail.com>
2019-01-09 18:05:08 -0500

    add functions to calculator

---
+++ calculator.py
@@ -0,0 +1,15 @@
+def multiply(a,b):
+    return a * b
+x = multiply(8,9)
+
+def add(a,b):
+    return a + b
+x = add(8,9)
+
+def subtract(a,b):
+    return a - b
+x = subtract(8,9)
+
+def divide(a,b):
+    return a / b
+x = divide(8,9)
