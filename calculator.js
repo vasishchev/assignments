c0914ed41f944ff07f69ab437ff62e45fa52f6b9
vasishchev <dmitry.vasishev@gmail.com>
2019-01-10 00:18:14 -0500

    call function and print return value and description

--- calculator.js
+++ calculator.js
@@ -13,3 +13,7 @@ function subtract(a,b) {
 function divide(a,b) {
 	return a / b
 }
+
+console.log("I'm going use the calculator functions to divide 47 by 42")
+var x = divide(47,42)
+console.log(x)
