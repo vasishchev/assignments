4377fcd8b2d4ce78868ac40ede49b361900a9b76
vasishchev <dmitry.vasishev@gmail.com>
2019-01-17 15:44:35 -0500

    add changes to group veggies exercise

--- pythondata/groupveggies.py
+++ pythondata/groupveggies.py
@@ -13,18 +13,18 @@ with open('vegetables.csv', 'r') as f:
 
 # Group vegtables by color as a variable vegtables_by_color.
 
-vegtables_by_color = {}
-for vegtable in vegetables:
-    color = vegtable['color']
-    if color in vegtables_by_color:
-        vegtables_by_color[color].append(vegtable)
+vegetables_by_color = {}
+for vegetable in vegetables:
+    color = vegetable['color']
+    if color in vegetables_by_color:
+        vegetables_by_color[color].append(vegetable)
     else:
-        vegtables_by_color[color] = [vegtable]
+        vegetables_by_color[color] = [vegetable]
 
-pprint(vegtables_by_color)
+pprint(vegetables_by_color)
 
 
 # Output vegtables_by_color into a json called vegtables_by_color.json.
 
-with open('vegtables_by_color.json', 'w') as f:
-    json.dump(vegtables_by_color, f, indent=2)
+with open('vegetables_by_color.json', 'w') as f:
+    json.dump(vegetables_by_color, f, indent=2)
