4b1effee44d99bb281307446877c124938c63a15
vasishchev <dmitry.vasishev@gmail.com>
2019-01-17 15:10:46 -0500

    add veggies exercise

---
+++ pythondata/filterveggies.py
@@ -0,0 +1,31 @@
+# Sarah and Dima
+
+# Read vegetables.csv into a variable called vegetables.
+
+import csv
+from pprint import pprint
+import json
+
+with open('vegetables.csv', 'r') as f:
+    reader = csv.DictReader(f)
+    vegetables = [dict(row) for row in reader] # Try with and without -- Convert Ordered Dict to regular dict (python 3.6 or higher)
+
+# Loop through vegetables and filter down to only green vegtables using a whitelist.
+
+green_vegetables = []
+whitelist = ['green']
+for veggie in vegetables:
+    if veggie['color'] in whitelist:
+        green_vegetables.append(veggie)
+
+# Print veggies to the terminal
+print(green_vegetables)
+
+# Write the veggies to a json file called greenveggies.json
+
+
+with open('greenveggies.json', 'w') as f:
+    json.dump(green_vegetables, f, indent=2)
+
+
+# Bonus: Output another csv called green_vegetables.csv.
--- pythondata/test.py
+++ pythondata/test.py
@@ -8,12 +8,30 @@
 #     writer.writerow(['val1', 'val2'])
 
 
-from datetime import datetime
+# DATES
+# from datetime import datetime
 
-raw_date = "2017-01-11"
-date_format = "%Y-%m-%d"
+# raw_date = "2017-01-11"
+# date_format = "%Y-%m-%d"
 
-# read date
-parsed_date = datetime.strptime(raw_date, date_format)
-date_str = parsed_date.strftime("%m/%d/%y") # 01/11/17
-print(date_str)
+# # read date
+# parsed_date = datetime.strptime(raw_date, date_format)
+# date_str = parsed_date.strftime("%m/%d/%y") # 01/11/17
+# print(date_str)
+
+
+# FILTERING
+friends = [
+    {"name": "Rachel", "age": 34},
+    {"name": "Monica", "age": 34},
+    {"name": "Phoebe", "age": 37}
+]
+
+# filter to age < 37
+cool_people = []
+whitelist = ['Rachel', 'Phoebe']
+for friend in friends:
+	if friend['name'] in whitelist:
+        cool_people.append(friend)
+
+print(cool_people)
