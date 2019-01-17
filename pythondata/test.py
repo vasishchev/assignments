420520cebc1c88953b24b2b67b67e335b34b67e2
vasishchev <dmitry.vasishev@gmail.com>
2019-01-17 11:26:42 -0500

    add csv module

--- pythondata/test.py
+++ pythondata/test.py
@@ -1,10 +1,8 @@
-# This example starts with a list, `mylist` and prints out the first element of the list. Note that lists in python are 0-indexed. The first element of the list is `1`.
-mylist = [1, 2, 3]
-print(mylist[0])
+import csv
 
-# Then, we append `4` to the end of the list and print the whole thing out. We can see the list now has 4 elements.
-mylist.append(4)
-print(mylist)
-
-# Last, we print out the element with index 2 (i.e. the 3rd element of the list which is 3).
-print(mylist[2])
+with open('testwrite.csv', 'w') as f:
+    writer = csv.writer(f)
+    writer.writerow(['col1', 'col2'])
+    writer.writerow(['val1', 'val2'])
+    writer.writerow(['val1', 'val2'])
+    writer.writerow(['val1', 'val2'])
---
+++ pythondata/veggies.py
@@ -0,0 +1,24 @@
+vegetables = [
+ {"name": "eggplant", "color": "purple"},
+ {"name": "tomato", "color": "red"},
+ {"name": "corn", "color": "yellow"},
+ {"name": "okra", "color": "green"},
+ {"name": "arugula", "color": "green"},
+ {"name": "broccoli", "color": "green"},
+]
+
+#write the vegetable to a CSV file
+
+import csv
+
+with open('vegetables.csv', 'w') as f:
+	writer = csv.writer(f)
+	#write header
+	writer.writerow(['name', 'color'])
+
+	#loop through each vegetable
+	for vegetable in vegetables:
+		name = vegetable['name']
+		color = vegetable['color']
+		length_of_name = len(name)
+		writer.writerow([name, color, length_of_name])
